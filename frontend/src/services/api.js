9660133bd9a321e505ad28bc8ae8a3bf27d391e8
hariraajan3 <ghariraajan@gmail.com>
2026-01-03 22:06:47 +0530

    updated api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const getBaseUrl = () => {
-  // let url = import.meta.env.VITE_BACKEND_API ;
-  let url='http://localhost:4000';
+   let url = import.meta.env.VITE_BACKEND_API ;
+  // let url='http://localhost:4000';
   if (!url.endsWith('/api')) url += '/api';
   return url;
 };
